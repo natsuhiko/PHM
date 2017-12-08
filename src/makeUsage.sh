ad398c10ed0e1a35399b2799608d401eaa2d72d1
Natsuhiko Kumasaka <nk5@sanger.ac.uk>
2017-12-08 22:06:11 +0000

    usage update

--- src/makeUsage.sh
+++ src/makeUsage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-grep -v "^#" usage_phm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\nvoid usage(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage_phm.c
-grep -v "^#" usage_bayeslm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\nvoid usage(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage_bayeslm.c
+grep -v "^#" usage_phm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\n\nvoid usage_phm(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage.c
+grep -v "^#" usage_hm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "\nvoid usage_hm(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' >> usage.c
+grep -v "^#" usage_bayeslm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "\nvoid usage_bayeslm(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' >> usage.c
---
+++ src/usage.h
@@ -0,0 +1,3 @@
+void usage_hm();
+void usage_phm();
+void usage_bayeslm();
