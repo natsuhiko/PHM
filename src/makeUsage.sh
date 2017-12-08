93c9811d4f3914a798b82a334f2b79c3f2b7a940
Natsuhiko Kumasaka <nk5@sanger.ac.uk>
2017-12-08 21:58:01 +0000

    usage upsage

--- src/makeUsage.sh
+++ src/makeUsage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-grep -v "^#" usage.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\nvoid usage(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage.c
+grep -v "^#" usage_phm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\nvoid usage(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage_phm.c
+grep -v "^#" usage_bayeslm.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\nvoid usage(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage_bayeslm.c
